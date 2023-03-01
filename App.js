fd48700e043df866038a2f2de5a51f5f375034a8
Letícia <102710369+leticiaqueirozd@users.noreply.github.com>
2023-02-28 21:47:42 -0300

    Desenvolvimento Mobile

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <Button onPress={() => {}}
+      title='Learn More'
+      color='blue'
+      accessibilityLabel='Learn more about this blue button'/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
